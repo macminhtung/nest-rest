7422187e9e37373e0dcd65ec121ee4edbc6e852d
Tung <tungmm@zigvy.com>
2025-11-27 23:37:02 +0700

    update header key to store the accessToken

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,9 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import type { TRequest } from '@/common/types';
-import { EMetadataKey } from '@/common/enums';
+import { EMetadataKey, ETokenType } from '@/common/enums';
 import { AuthService } from '@/modules/auth/auth.service';
-import { ETokenType } from '@/modules/shared/services';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -15,7 +14,7 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     // Get token from request
     const request = context.switchToHttp().getRequest<TRequest>();
-    const accessToken = request.headers?.authorization?.replace('Bearer ', '') || '';
+    const accessToken = `${request.headers?.['xt-sol-api-key']}`.replace('Bearer ', '') || '';
 
     // #==========================#
     // # ==> CASE: PUBLIC API <== #
