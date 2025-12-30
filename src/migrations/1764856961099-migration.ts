97589f64c78278e1696d72c84ba74b7cef617376
Tung <tungmm@zigvy.com>
2025-12-30 22:27:07 +0700

    update ACCESS_TOKEN_EXPIRES_IN value and comment the Migration1764856961099 file

--- src/migrations/1764856961099-migration.ts
+++ src/migrations/1764856961099-migration.ts
@@ -1,49 +1,49 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+// import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migration1764856961099 implements MigrationInterface {
-  name = 'Migration1764856961099';
+// export class Migration1764856961099 implements MigrationInterface {
+//   name = 'Migration1764856961099';
 
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `CREATE TYPE "public"."role_name_enum" AS ENUM('admin', 'staff', 'user')`,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "role" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" integer NOT NULL, "name" "public"."role_name_enum" NOT NULL, "description" character varying, CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE TYPE "public"."user_token_type_enum" AS ENUM('ACCESS_TOKEN', 'REFRESH_TOKEN')`,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "user_token" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" uuid NOT NULL, "hash_token" character varying NOT NULL, "type" "public"."user_token_type_enum" NOT NULL, "refresh_token_id" uuid, "user_id" uuid NOT NULL, CONSTRAINT "UQ_f45f28a3a739d71c52d2f06cc84" UNIQUE ("refresh_token_id"), CONSTRAINT "PK_48cb6b5c20faa63157b3c1baf7f" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IDX_195d98fb5e96f44b4588cd8f45" ON "user_token" ("user_id", "type", "hash_token") `,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "user" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" uuid NOT NULL, "avatar" character varying(500) NOT NULL DEFAULT '', "email" character varying NOT NULL, "password" character varying NOT NULL, "first_name" character varying(100) NOT NULL, "last_name" character varying(100) NOT NULL, "is_email_verified" boolean NOT NULL DEFAULT false, "role_id" integer NOT NULL, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `CREATE TABLE "product" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" uuid NOT NULL, "image" character varying NOT NULL, "name" character varying(100) NOT NULL, "description" character varying(1000) NOT NULL, CONSTRAINT "UQ_22cc43e9a74d7498546e9a63e77" UNIQUE ("name"), CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "user_token" ADD CONSTRAINT "FK_79ac751931054ef450a2ee47778" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "user" ADD CONSTRAINT "FK_fb2e442d14add3cefbdf33c4561" FOREIGN KEY ("role_id") REFERENCES "role"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    );
-  }
+//   public async up(queryRunner: QueryRunner): Promise<void> {
+//     await queryRunner.query(
+//       `CREATE TYPE "public"."role_name_enum" AS ENUM('admin', 'staff', 'user')`,
+//     );
+//     await queryRunner.query(
+//       `CREATE TABLE "role" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" integer NOT NULL, "name" "public"."role_name_enum" NOT NULL, "description" character varying, CONSTRAINT "PK_b36bcfe02fc8de3c57a8b2391c2" PRIMARY KEY ("id"))`,
+//     );
+//     await queryRunner.query(
+//       `CREATE TYPE "public"."user_token_type_enum" AS ENUM('ACCESS_TOKEN', 'REFRESH_TOKEN')`,
+//     );
+//     await queryRunner.query(
+//       `CREATE TABLE "user_token" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" uuid NOT NULL, "hash_token" character varying NOT NULL, "type" "public"."user_token_type_enum" NOT NULL, "refresh_token_id" uuid, "user_id" uuid NOT NULL, CONSTRAINT "UQ_f45f28a3a739d71c52d2f06cc84" UNIQUE ("refresh_token_id"), CONSTRAINT "PK_48cb6b5c20faa63157b3c1baf7f" PRIMARY KEY ("id"))`,
+//     );
+//     await queryRunner.query(
+//       `CREATE INDEX "IDX_195d98fb5e96f44b4588cd8f45" ON "user_token" ("user_id", "type", "hash_token") `,
+//     );
+//     await queryRunner.query(
+//       `CREATE TABLE "user" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" uuid NOT NULL, "avatar" character varying(500) NOT NULL DEFAULT '', "email" character varying NOT NULL, "password" character varying NOT NULL, "first_name" character varying(100) NOT NULL, "last_name" character varying(100) NOT NULL, "is_email_verified" boolean NOT NULL DEFAULT false, "role_id" integer NOT NULL, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+//     );
+//     await queryRunner.query(
+//       `CREATE TABLE "product" ("created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP DEFAULT now(), "deleted_at" TIMESTAMP, "id" uuid NOT NULL, "image" character varying NOT NULL, "name" character varying(100) NOT NULL, "description" character varying(1000) NOT NULL, CONSTRAINT "UQ_22cc43e9a74d7498546e9a63e77" UNIQUE ("name"), CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`,
+//     );
+//     await queryRunner.query(
+//       `ALTER TABLE "user_token" ADD CONSTRAINT "FK_79ac751931054ef450a2ee47778" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+//     );
+//     await queryRunner.query(
+//       `ALTER TABLE "user" ADD CONSTRAINT "FK_fb2e442d14add3cefbdf33c4561" FOREIGN KEY ("role_id") REFERENCES "role"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+//     );
+//   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_fb2e442d14add3cefbdf33c4561"`);
-    await queryRunner.query(
-      `ALTER TABLE "user_token" DROP CONSTRAINT "FK_79ac751931054ef450a2ee47778"`,
-    );
-    await queryRunner.query(`DROP TABLE "product"`);
-    await queryRunner.query(`DROP TABLE "user"`);
-    await queryRunner.query(`DROP INDEX "public"."IDX_195d98fb5e96f44b4588cd8f45"`);
-    await queryRunner.query(`DROP TABLE "user_token"`);
-    await queryRunner.query(`DROP TYPE "public"."user_token_type_enum"`);
-    await queryRunner.query(`DROP TABLE "role"`);
-    await queryRunner.query(`DROP TYPE "public"."role_name_enum"`);
-  }
-}
+//   public async down(queryRunner: QueryRunner): Promise<void> {
+//     await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_fb2e442d14add3cefbdf33c4561"`);
+//     await queryRunner.query(
+//       `ALTER TABLE "user_token" DROP CONSTRAINT "FK_79ac751931054ef450a2ee47778"`,
+//     );
+//     await queryRunner.query(`DROP TABLE "product"`);
+//     await queryRunner.query(`DROP TABLE "user"`);
+//     await queryRunner.query(`DROP INDEX "public"."IDX_195d98fb5e96f44b4588cd8f45"`);
+//     await queryRunner.query(`DROP TABLE "user_token"`);
+//     await queryRunner.query(`DROP TYPE "public"."user_token_type_enum"`);
+//     await queryRunner.query(`DROP TABLE "role"`);
+//     await queryRunner.query(`DROP TYPE "public"."role_name_enum"`);
+//   }
+// }
